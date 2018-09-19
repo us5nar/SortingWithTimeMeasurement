7ae9ded74913f569138431450c110d27778fe0dd
Anton Bielov <us5nar@gmail.com>
2018-09-19 22:46:32 +0300

    1st commit - created structure of the solution

---
+++ ISorter/ISorter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingWithTimeMeasurement
+{
+    interface ISorter
+    {
+        // interface definition
+        // interface members:
+        // Method Sort with 2 overloads
+        // Sort the array
+        int[] Sort(int[] arrayToSort);
+        // Sort input array and measure sorting time
+        int[] Sort(int[] arrayToSort, out int sortingTime);
+    }
+}
