83234a8ad67b5c40c0411daf4356cb163b9ae39e
Anton Bielov <us5nar@gmail.com>
2018-09-30 17:30:04 +0300

    remake the generator

--- ArrayGenerator/ArrayGenerator.cs
+++ ArrayGenerator/ArrayGenerator.cs
@@ -8,6 +8,17 @@ namespace ArrayGenerator
 {
     class ArrayGenerator
     {
-       
+        private static Random r = new Random();
+
+        public static int[] GetRandomArray(int arrySize)
+        {
+            SortedDictionary<double, int> sortedSet = new SortedDictionary<double, int>();
+
+            for (int index = 0; index < arrySize; index++)
+            {
+                sortedSet.Add(r.NextDouble(), index);
+            }
+            return sortedSet.Select(x => x.Value).ToArray();
+        }
     }
 }
