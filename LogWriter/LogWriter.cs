7cc4576b33f9665bbd8bd3a89f1a6c073f6b99fb
Anton Bielov <us5nar@gmail.com>
2018-10-08 22:50:49 +0300

    Log writer is static now!!!!

--- LogWriter/LogWriter.cs
+++ LogWriter/LogWriter.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 
 namespace LogWriterNameSpace
 {
-    public class LogWriter
+    public static class LogWriter
     {
         //Contains Logfilename with current path
-        private string currenExePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private static string currenExePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         //Contains formatted day with time
-        private string timeStamp = DateTime.Now.ToString("dd-MM-yyyy | HH:mm:ss | ");
+        private static string timeStamp = DateTime.Now.ToString("dd-MM-yyyy | HH:mm:ss | ");
         //Appends log string with DayTime stamp
-        public void Write(string logMessage)           
+        public static void Write(string logMessage)           
         {            
             try
             {
--- SortingWithTimeMeasurement/Program.cs
+++ SortingWithTimeMeasurement/Program.cs
@@ -9,18 +9,16 @@ namespace SortingWithTimeMeasurement
     {
         static void Main(string[] args)
         {
-            LogWriter logWriter = new LogWriter();
+            //LogWriter logWriter = new LogWriter();
             int[] unsortedArray = ArrayGenerator.GetRandomArray(10);
             //An exapmple of log writer usage
             //-----------I want to try it as a static-------------
-            logWriter.Write("====================!!!START!!!=======================");
+            LogWriter.Write("====================!!!START!!!=======================");
             foreach (int element in unsortedArray)
             {
-                logWriter.Write(element.ToString());
+                LogWriter.Write(element.ToString());
             }
-            logWriter.Write("====================!!!STOP!!!=======================");
-
-
+            LogWriter.Write("====================!!!STOP!!!=======================");
 
             Console.ReadLine();
 
