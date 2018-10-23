6f9166e62e5855805db616902fb5d33d40a05f9e
Anton Bielov <us5nar@gmail.com>
2018-10-24 00:58:10 +0300

    Added defautm sorter realization. Need help with Sort method

--- DefaultSorter/DefaultSorter.cs
+++ DefaultSorter/DefaultSorter.cs
@@ -1,17 +1,18 @@
-﻿using ISorterNameSpace;
+﻿using System;
+using ISorterNameSpace;
 namespace DefaultSorter
 {
     public class DefaultSorter : ISorter
 
     {
-        public int[] Sort(int[] arrayToSort)
-        {
-            throw new System.NotImplementedException();
-        }
+        
 
         public int[] Sort(int[] arrayToSort, out int sortingTime)
         {
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
+            sortingTime = 1;
+            return arrayToSort; //somethig wrong with Sort method
+            
         }
     }
 }
