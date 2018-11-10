b86dd284209c6daff299104ee822f8be94b139cb
Anton Bielov <us5nar@gmail.com>
2018-11-11 01:05:42 +0200

    Added IncerionSort project. AI - realize IncertionSorter class as an Isorter child.

---
+++ IcertionSorter/IcertionSorter/InsertionSorter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSorterNaneSpace
+{
+    class InsertionSorter
+    {
+        public static int[] Sort(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > inputArray[j])
+                    {
+                        int temp = inputArray[j - 1];
+                        inputArray[j - 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+            return inputArray;
+        }
+    }
+}
