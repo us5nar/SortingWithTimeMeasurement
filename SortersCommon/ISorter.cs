478ed0c1da2ad5cdd712753c431bd3af2b4d4e11
Anton Bielov <us5nar@gmail.com>
2019-02-19 00:34:14 +0200

    tryied to rework WinForm paret as MVxxx

--- SortersCommon/ISorter.cs
+++ SortersCommon/ISorter.cs
@@ -1,9 +1,18 @@
-﻿namespace SortersCommon
+﻿using System;
+namespace SortersCommon
 {
     public interface ISorter
     {
         // interface definition
         // Sorts input array and measure sorting time
-        int[,] Sort(int[,] arrayToSort, out double sortingTimeMs);
+        //int[,] Sort(int[,] arrayToSort, out double sortingTimeMs);
+
+        event EventHandler SortingStart;
+        event EventHandler<SortingIterationEndedEventArgs> SortingIterationEnded;
+        event EventHandler<SortingEndedEventArgs> SortingEnd;
+
+
+        int[] Sort(int[] arrayToSort);
+
     }
 }
